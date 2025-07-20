f216c177942ef2dc783b6c497e0dbb201e900d90
miguelfinda <154908251+miguelfinda@users.noreply.github.com>
2025-07-20 18:57:47 +0100

    Add files via upload
    primeira subida

---
+++ assets/js/accao.js
@@ -0,0 +1,17 @@
+
+    var botao = document.querySelector("#btn")
+    var m = document.querySelector(".menu")
+    botao.addEventListener("click", function () {
+
+
+      m.classList.toggle("show");
+
+
+
+
+
+    })
+
+
+
+
