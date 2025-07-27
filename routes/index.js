829ac791473261300ca1208dd89e91969bc3ba8e
Manuel <nicocohen56@gmail.com>
2025-07-27 18:23:03 +0100

    adicionei um exemplo de renderizaçao(rota)da pagina divulgar

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 
-router.get('/', mainController.home);
-router.get('/artistas', mainController.artistas);
-router.get('/categoria', mainController.categoria);
+router.get('/', mainController.home);//exemplo de rota para a página inicial
+
+router.get('/divulgar', function(req, res) {
+  res.render('pages/divulgar', { title: 'Divulgar' });//exemplo de renderização da página divulgar
+});
+
 
 module.exports = router;
